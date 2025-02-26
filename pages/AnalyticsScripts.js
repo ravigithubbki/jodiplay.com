31aff10afd3899cacf9550966be3eb9e8f5a83f6
ravigithubbki <118995791+ravigithubbki@users.noreply.github.com>
2025-02-26 22:56:54 +0530

    Update AnalyticsScripts.js

--- pages/AnalyticsScripts.js
+++ pages/AnalyticsScripts.js
@@ -4,30 +4,21 @@ const AnalyticsScripts = () => {
   return (
     <Head>
       {/* Meta Pixel Code */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-           !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1296648814800748');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1473907386628360&ev=PageView&noscript=1"
-        />
-      </noscript>
+      <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1536254820374324');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1536254820374324&ev=PageView&noscript=1"
+/></noscript>
       {/* End Meta Pixel Code */}
 
       {/* Google Analytics */}
